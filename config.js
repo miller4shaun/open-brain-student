599037d3134713e5994a0e6461335215575bf7c7
miller4shaun <miller4shaun@gmail.com>
2026-09-14 22:08:07 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-// Your Supabase project details — replace both values below.
-// Find them at: supabase.com → your project → Settings → API
-
+// Your Supabase project details
 const SUPABASE_URL = 'https://jvgalzevrdbcskuzcxwn.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2Z2FsemV2cmRiY3NrdXpjeHduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk0MzgwMzQsImV4cCI6MjEwNTAxNDAzNH0.DzJV4-n67lBvcnYehKIVuMH1h5aXhIzrrCW1fp_dX8s'
