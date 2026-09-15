a021214855ca01f8e379f2ded112b8d400477913
miller4shaun <miller4shaun@gmail.com>
2026-09-14 22:04:23 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 // Find them at: supabase.com → your project → Settings → API
 
 const SUPABASE_URL = 'https://jvgalzevrdbcskuzcxwn.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2Z2FsemV2cmRiY3NrdXpjeHduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk0MzgwMzQsImV4cCI6MjEwNTAxNDAzNH0.DzJV4-n67lBvcnYehKIVuMH1h5aXhIzrrCW1fp_dX8s '
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2Z2FsemV2cmRiY3NrdXpjeHduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk0MzgwMzQsImV4cCI6MjEwNTAxNDAzNH0.DzJV4-n67lBvcnYehKIVuMH1h5aXhIzrrCW1fp_dX8s'
